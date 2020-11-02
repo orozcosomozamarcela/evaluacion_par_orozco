667eef17292d2918130fd1d1e05ac8b07cfd440a
Marcela Orozco <orozcosomozamarcela@gmail.com>
2020-11-02 19:49:26 -0300

    continuando la evaluacion

--- evaluacion_par.py
+++ evaluacion_par.py
@@ -1,1 +1,10 @@
 import csv
+
+def menu():
+    print('1. Guardar datos empleados')
+    print('2. Leer Datos empleados ')
+    print('0. Salir')
+
+def menu_secundario():
+    print('1. ¿Quiere Continuar Cargando Datos en CSV?')
+    print('0. Salir')
